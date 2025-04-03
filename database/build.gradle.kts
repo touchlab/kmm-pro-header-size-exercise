d6b9349afea9af87f3a0f715f5884748febe42df
Kevin Schildhorn <kevin.schildhorn@gmail.com>
2025-04-03 09:49:16 -0400

    Adding Database

---
+++ database/build.gradle.kts
@@ -0,0 +1,80 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSetTree
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.sqlDelight)
+}
+
+android {
+    namespace = "co.touchlab.kampkit.database"
+    compileSdk = libs.versions.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+    }
+    @Suppress("UnstableApiUsage")
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
+
+    lint {
+        warningsAsErrors = true
+        abortOnError = true
+    }
+}
+
+version = "1.2"
+
+kotlin {
+    jvmToolchain(11)
+    // https://kotlinlang.org/docs/multiplatform-expect-actual.html#expected-and-actual-classes
+    // To suppress this warning about usage of expected and actual classes
+    @OptIn(ExperimentalKotlinGradlePluginApi::class)
+    compilerOptions {
+        freeCompilerArgs.add("-Xexpect-actual-classes")
+    }
+    androidTarget {
+        @Suppress("OPT_IN_USAGE")
+        unitTestVariant.sourceSetTree.set(KotlinSourceSetTree.test)
+    }
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64(),
+    ).forEach {
+        it.binaries.framework {
+            isStatic = false
+            linkerOpts("-lsqlite3")
+            export(libs.touchlab.kermit.simple)
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.sqlDelight.coroutinesExt)
+            api(libs.touchlab.kermit)
+        }
+        commonTest.dependencies {
+            implementation(libs.bundles.shared.commonTest)
+        }
+        androidMain.dependencies {
+            implementation(libs.sqlDelight.android)
+        }
+        getByName("androidUnitTest").dependencies {
+            implementation(libs.bundles.shared.androidTest)
+        }
+        iosMain.dependencies {
+            implementation(libs.sqlDelight.native)
+            api(libs.touchlab.kermit.simple)
+        }
+    }
+}
+
+sqldelight {
+    databases.create("KaMPKitDb") {
+        packageName.set("co.touchlab.kampkit.db")
+    }
+}
